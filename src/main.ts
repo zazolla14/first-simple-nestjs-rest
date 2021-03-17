3bc0286abfbf1c3a8b318ecc27fde356a149e9f0
Azola Zubizarreta <za.ntik14@gmail.com>
2021-03-18 00:01:35 +0700

    finished tutorial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { UsersModule } from './users/users.module';
+import "dotenv/config"; //! import file .ENV
+
+import { NestFactory } from "@nestjs/core";
+import { UsersModule } from "./users/users.module";
+import { Logger } from "@nestjs/common";
+
+const port = process.env.PORT; //! define PORT dari file .ENV
 
 async function bootstrap() {
   const app = await NestFactory.create(UsersModule);
-  await app.listen(3000);
+  await app.listen(port); //! Port sesuai dengan file .ENV
+
+  Logger.log(`Running at port ${port}`, "RunningPort");
 }
 bootstrap();
