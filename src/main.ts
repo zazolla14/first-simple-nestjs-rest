2a68992fca59f93681f7dca9613315c103b352b2
Azola Zubizarreta <za.ntik14@gmail.com>
2021-03-18 00:35:04 +0700

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
-import "dotenv/config"; //! import file .ENV
-
 import { NestFactory } from "@nestjs/core";
 import { UsersModule } from "./users/users.module";
 import { Logger } from "@nestjs/common";
 
-const port = process.env.PORT; //! define PORT dari file .ENV
-
 async function bootstrap() {
   const app = await NestFactory.create(UsersModule);
-  await app.listen(port); //! Port sesuai dengan file .ENV
+  await app.listen(3000);
 
-  Logger.log(`Running at port ${port}`, "RunningPort");
+  Logger.log(`Running at port 3000`, "RunningPort");
 }
 bootstrap();
